d8e8cd9f760036748031af5c9fcc29e6bef96a5d
Zizikora <z13476354615@outlook.com>
2023-04-05 15:30:34 -0500

    basic javascript

---
+++ hello.js
@@ -0,0 +1,37 @@
+function sayHello() {
+    var x = 5; 
+var y = 10; 
+var sum = x + y;
+alert(sum);
+}
+
+// Call function on click of the button
+document.getElementById("myBtn").onclick = sayHello;
+
+
+
+// Call function on click of the button
+document.getElementById("myBtn").onclick = sayHello;
+
+var x = 5; 
+var y = 10; 
+var sum = x + y; 
+// This is my first JavaScript program
+var name = "pita";
+var age = 21;
+var isMarried = false;
+// Declaring Variable
+var userName;
+ 
+// Assigning value
+userName = "Clark Kent";
+// Declaring multiple Variables
+var name = "Peter Parker", age = 21, isMarried = false;
+ 
+/* Longer declarations can be written to span
+multiple lines to improve the readability */
+var name = "Peter Parker",
+age = 21,
+isMarried = false;
+
+
